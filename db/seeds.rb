f5be17434e38606f2997adac4a540e73a235d087
Nastassja Swierczynski <nastassja.swierczynski@gmail.com>
2019-05-20 17:18:02 +0200

    task completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  { name: 'Dishoom', address: '7 Sunshine St, London E2', category: 'chinese' },
+  { name: 'Shoom', address: '7 Rain St, Paris', category: 'italian' },
+  { name: 'Dis', address: '7 Rainbow St, Berlin', category: 'japanese' },
+  { name: 'Didida', address: '7 Bound St, New York ', category: 'french' },
+  { name: 'Irara', address: '7 Sunday St, London', category: 'belgian' }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
